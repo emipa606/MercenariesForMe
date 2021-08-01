32168f41b79806225ca7c56157177e56b23aecf8
aRandomKiwi <15568941+aRandomKiwi@users.noreply.github.com>
2021-08-01 08:28:26 +0200

    Add support for Rimworld 1.3

---
+++ Source/1.3/Harmony/Pawn_Patch.cs
@@ -0,0 +1,38 @@
+﻿using Verse;
+using Verse.AI;
+using Verse.AI.Group;
+using HarmonyLib;
+using RimWorld;
+using System.Linq;
+
+namespace aRandomKiwi.MFM
+{
+    internal class Pawn_Patch
+    {
+        [HarmonyPatch(typeof(Pawn), "Kill")]
+        public class Kill
+        {
+            [HarmonyPostfix]
+            public static void Listener(Pawn __instance, DamageInfo? dinfo, Hediff exactCulprit = null)
+            {
+                //If VIP killed stop the job of guardian of all the mercenaries the guardan
+                if (__instance != null)
+                {
+                    Comp_USFM comp = __instance.TryGetComp<Comp_USFM>();
+                    if (comp != null)
+                    {
+
+                        if (comp.Level == MercenaryLevel.Cyborg)
+                        {
+                            //If cyborg belonging to the player ==> removal of all organs
+                            foreach (var h in __instance.health.hediffSet.GetHediffs<HediffWithComps>().ToList())
+                            {
+                                __instance.health.hediffSet.hediffs.Remove(h);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Source/1.3/Other/MainTabWindow_Synth.cs
@@ -0,0 +1,18 @@
+﻿using RimWorld.Planet;
+using System;
+using Verse;
+using Verse.Sound;
+using RimWorld;
+
+namespace aRandomKiwi.MFM
+{
+    public class MainButtonWorker_Synth : MainButtonWorker
+    {
+        public override void Activate()
+        {
+            ChoiceLetter_Bill cl = new ChoiceLetter_Bill();
+            cl.init();
+            Find.WindowStack.Add(new Bill(cl, true));
+        }
+    }
+}
